548b01d02a90cb96188e0e9756b27c4426a9a3df
Lang <langdeng@hotmail.com>
2020-02-13 21:31:52 -0800

    Added a comment

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -4,6 +4,7 @@ import numpy as np     # installed with matplotlib
 import matplotlib.pyplot as plt
 
 def main():
+    # a np works with plot
     x = np.arange(0, radians(1800), radians(12))
     plt.plot(x, np.cos(x), 'b')
     plt.show()
